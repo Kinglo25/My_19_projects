3377fc2c89ed5466ced14ca276b4f0022dcec104
Loic Majerus <59590960+Kinglo25@users.noreply.github.com>
2022-10-01 16:44:50 +0200

    Update ft_push.c

--- rendu/libft/ft_push.c
+++ rendu/libft/ft_push.c
@@ -1,1 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_push.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Loic Majerus <loic.majerus@gmail.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/01 15:31:17 by Loic Majeru       #+#    #+#             */
+/*   Updated: 2022/10/01 15:31:17 by Loic Majeru      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "libft.h"
+
+int main()
+{
+    int x = 0;
+    t_list *test = malloc(sizeof(t_list));
+
+    while (x < 5)
+    {
+        push(&test, x++);
+    }
+    while (test)
+    {
+        printf("%i\n", test->content);
+    }
+}
+
+void    push(t_list **head, void *content)
+{
+    t_list *newNode;
+
+    newNode = malloc(sizeof(t_list));
+    if (!newNode)
+        exit(0);
+    newNode->content = content;
+    newNode->next = *head;
+    *head = newNode;
+}
